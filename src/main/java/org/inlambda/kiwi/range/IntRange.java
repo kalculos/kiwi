6b11f3be42bc8f66b976567c6dfb8e6e57ba13f0
iceBear67 <icebear67@sfclub.cc>
2022-04-23 18:04:08 +0800

    Add IntRange#random

--- src/main/java/org/inlambda/kiwi/range/IntRange.java
+++ src/main/java/org/inlambda/kiwi/range/IntRange.java
@@ -1,24 +1,55 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 InlinedLambdas and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package org.inlambda.kiwi.range;
 
 import lombok.RequiredArgsConstructor;
+import org.inlambda.kiwi.RandomHelper;
 
 @RequiredArgsConstructor
 public final class IntRange {
     private final int atLeast;
-    private final int atMost ;
+    private final int atMost;
 
-    public boolean isInRange(int i){
+    public boolean isInRange(int i) {
         return atLeast <= i && i <= atMost;
     }
 
-    public static IntRange rangeOf(int atLeast,int atMost){
-        return new IntRange(atLeast,atMost);
+    public int random() {
+        return RandomHelper.number(atLeast, atMost);
     }
-    public static IntRange rangeAtMost(int atMost){
-        return new IntRange(Integer.MIN_VALUE,atMost);
+
+    public static IntRange rangeOf(int atLeast, int atMost) {
+        return new IntRange(atLeast, atMost);
     }
-    public static IntRange rangeAtLeast(int atLeast){
-        return new IntRange(atLeast,Integer.MAX_VALUE);
+
+    public static IntRange rangeAtMost(int atMost) {
+        return new IntRange(Integer.MIN_VALUE, atMost);
+    }
+
+    public static IntRange rangeAtLeast(int atLeast) {
+        return new IntRange(atLeast, Integer.MAX_VALUE);
     }
     private static final IntRange POSITIVE = new IntRange(1,Integer.MAX_VALUE);
     public static IntRange rangePositive(){
