2e2924b07209dc11e3aca01331f4734fe3106a99
iceBear67 <icebear67@sfclub.cc>
2023-03-18 20:51:33 +0800

    remove collection-bukkit

--- collection-bukkit/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module kiwi.collection.bukkit {
-    exports io.ib67.kiwi.bukkit;
-    exports io.ib67.kiwi.collection.bukkit;
-    exports io.ib67.kiwi.collection.bukkit.maps;
-    requires lombok;
-    requires org.bukkit;
-    requires org.jetbrains.annotations;
-    requires com.google.gson;
-}
