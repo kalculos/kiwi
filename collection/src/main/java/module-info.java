cc26bd1c4e96bb8495b92952192d4394214c6f95
iceBear67 <icebear67@sfclub.cc>
2025-06-16 16:57:01 +0800

    bump version and remove unuseful classes.

--- collection/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module kiwi.collection {
-    exports io.ib67.kiwi.collection.list;
-    exports io.ib67.kiwi.collection.iterator;
-    exports io.ib67.kiwi.collection.set;
-    requires org.jetbrains.annotations;
-    requires kiwi.core;
-    requires lombok;
-}
--- lang/src/main/java/module-info.java
+++ lang/src/main/java/module-info.java
@@ -2,9 +2,7 @@ module kiwi.core {
     exports io.ib67.kiwi.lazy;
     exports io.ib67.kiwi;
     exports io.ib67.kiwi.reflection;
-    exports io.ib67.kiwi.tuple;
     exports io.ib67.kiwi.lock;
-    exports io.ib67.kiwi.future;
     exports io.ib67.kiwi.exception;
     requires org.jetbrains.annotations;
     requires lombok;
