adb1a243accf94d2926f2131451a4701144ca2af
iceBear67 <icebear67@sfclub.cc>
2025-06-17 18:30:10 +0800

    clean up

--- lang/src/main/java/io/ib67/kiwi/RandomHelper.java
+++ lang/src/main/java/io/ib67/kiwi/RandomHelper.java
@@ -34,17 +34,6 @@ import java.util.concurrent.ThreadLocalRandom;
 @UtilityClass
 public final class RandomHelper {
 
-    @SuppressWarnings("unchecked")
-    public static <T> T pickOrNull(Collection<T> t) {
-        if (t.size() == 0) return null;
-        return (T) t.toArray()[ThreadLocalRandom.current().nextInt(t.size())];
-    }
-
-    public static <T> T pickOrNull(List<T> t) {
-        if (t.size() == 0) return null;
-        return t.get(ThreadLocalRandom.current().nextInt(t.size()));
-    }
-
     public static int number(int bound) {
         return ThreadLocalRandom.current().nextInt(bound);
     }
